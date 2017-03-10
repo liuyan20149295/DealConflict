fa02ee13dd57374c2e9a21401bcf9b87a82935ca
LiuYan2016 <liuyan5@cmos.chinamobile.com>
2017-03-10 16:42:25 +0800

    commit

---
+++ src/main/java/com/cmos/conflict/TestDemo.java
@@ -0,0 +1,10 @@
+package com.cmos.conflict;
+
+/**
+ * Created by Lenovo on 2017/3/10.
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("testdemo1");
+    }
+}
